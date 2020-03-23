1cc664fccd0eb2ffa483bfb70c66c5dd5c79ee93
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-03-23 10:03:52 +0100

    Initial Commit

---
+++ SinglePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SinglePhoto
+//
+//  Created by wilson on 3/23/20.
+//  Copyright © 2020 wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
